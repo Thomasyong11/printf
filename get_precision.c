94f6d333898bb166ea547a8fd4fe9c09ee7b7e07
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-01-22 00:05:44 +0200

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -18,15 +18,12 @@ int get_precision(const char *format, int *i, va_list list)
 
 	precision = 0;
 
-
-int get_precision(const char *format, int *i, va_list list)
-
-{
-
-		int curr_i = *i + 1;
-
-			int precision = -1;
-
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
 
 
 				if (format[curr_i] != '.')
