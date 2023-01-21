250eda512a97433264517d776e7f545a9dbfa086
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-01-22 00:21:04 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -68,119 +68,19 @@ long int convert_size_number(long int num, int size)
 	return ((int)num);
 }
 
-
-
-
-				buffer[i++] = '\\';
-
-					buffer[i++] = 'x';
-
-
-
-						buffer[i++] = map_to[ascii_code / 16];
-
-							buffer[i] = map_to[ascii_code % 16];
-
-
-
-								return (3);
-
-}
-
-
-
 /**
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  *
- *  * is_digit - Verifies if a char is a digit
- *
- *   * @c: Char to be evaluated
- *
- *    *
- *
- *     * Return: 1 if c is a digit, 0 otherwise
- *
- *      */
-
-int is_digit(char c)
-
-{
-
-		if (c >= '0' && c <= '9')
-
-					return (1);
-
-
-
-			return (0);
-
-}
-
-
-
-/**
- *
- *  * convert_size_number - Casts a number to the specified size
- *
- *   * @num: Number to be casted.
- *
- *    * @size: Number indicating the type to be casted.
- *
- *     *
- *
- *      * Return: Casted value of num
- *
- *       */
-
-long int convert_size_number(long int num, int size)
-
-{
-
-		if (size == S_LONG)
-
-					return (num);
-
-			else if (size == S_SHORT)
-
-						return ((short)num);
-
-
-
-				return ((int)num);
-
-}
-
-
-
-/**
- *
- *  * convert_size_unsgnd - Casts a number to the specified size
- *
- *   * @num: Number to be casted
- *
- *    * @size: Number indicating the type to be casted
- *
- *     *
- *
- *      * Return: Casted value of num
- *
- *       */
-
+ * Return: Casted value of num
+ */
 long int convert_size_unsgnd(unsigned long int num, int size)
-
 {
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
 
-		if (size == S_LONG)
-
-					return (num);
-
-			else if (size == S_SHORT)
-
-						return ((unsigned short)num);
-
-
-
-				return ((unsigned int)num);
-
+	return ((unsigned int)num);
 }
-
-
