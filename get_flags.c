d103266924952049c7b3def44cce58568bbaf91e
Cleopatrakadzinga <118051305+Cleopatrakadzinga@users.noreply.github.com>
2023-01-22 16:50:32 +0200

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * print_unsigned - Prints an unsigned number
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
  */
+
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
